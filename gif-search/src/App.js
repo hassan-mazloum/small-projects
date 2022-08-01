cfdc6ae1aa7d80bb97ec9bf952a019962f293c87
hassan-mazloum <hass.jmazloum@gmail.com>
2022-08-01 12:07:58 -0400

    Styled Gif image search from Gif API using React hooks

---
+++ gif-search/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+import SearchForm from "./SearchForm";
+import GifList from "./GifList";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("webdeveloper");
+  const [isloading, setIsLoading] = useState(true);
+
+  const performSearch = (value) => setQuery(value);
+  // fetching data from giphy api
+  useEffect(() => {
+    axios(
+      `https://api.giphy.com/v1/gifs/search?api_key=f3b8PiHqEC58PxYkUN0KluDjNK6pTufN&q=${query}&limit=25&offset=0&rating=g&lang=en`
+    )
+      .then((res) => setData(res.data.data))
+      .catch((error) => console.log("Error fetching and parsing data", error))
+      .finally(() => setIsLoading(false));
+  }, [query]);
+
+  return (
+    <>
+      <div className="main-header">
+        <div className="inner">
+          <h1 className="main-title">GifSearch</h1>
+          <SearchForm onSearch={performSearch} />
+        </div>
+      </div>
+      <div className="main-content">
+        {isloading ? <p>Loading...</p> : <GifList data={data} />}
+      </div>
+    </>
+  );
+}
+
+export default App;
